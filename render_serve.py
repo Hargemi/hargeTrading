24df6f9f93c3b5a9fc04638ff4eba66af0e42e24
aley jose <aleyjose755@gmail.com>
2025-07-31 19:17:40 -0300

    Actualizacion para error 500

---
+++ render_serve.py
@@ -0,0 +1,9 @@
+import os
+from waitress import serve
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'harge.settings')
+application = get_wsgi_application()
+
+port = int(os.environ.get('PORT', 10000))
+serve(application, host='0.0.0.0', port=port)
