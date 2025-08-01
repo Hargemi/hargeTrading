6ad1f82c48a6528f0d6f0170f84053a485e1ca60
aley jose <aleyjose755@gmail.com>
2025-07-31 21:02:40 -0300

    Actualizacion para error 500 por 8 ves

--- harge/settings.py
+++ harge/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY', default='your-default-secret-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.onrender.com']
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
